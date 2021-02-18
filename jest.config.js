ddb847b717399ace9282031e980f69614d80579f
Antonio Manuel Perez Lopez <aperezl@gmail.com>
2021-02-18 17:32:39 +0100

    added jest

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/en/configuration.html
+ */
+
+module.exports = {
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  testEnvironment: 'node'
+}
