f3a4ea18e369c8b28457c21be65f4841e7f22a88
Antonio Manuel Perez Lopez <aperezl@gmail.com>
2021-02-19 01:14:53 +0100

    ensure LoginRouter returns 500 if an invalid AuthUseCase is provided

--- src/presentation/routers/LoginRouter.spec.js
+++ src/presentation/routers/LoginRouter.spec.js
@@ -79,4 +79,28 @@ describe('Login Router', () => {
     expect(httpResponse.statusCode).toBe(401)
     expect(httpResponse.body).toEqual(new UnauthorizedError())
   })
+
+  test('should return 500 if not AuthUseCase is provided', () => {
+    const sut = new LoginRouter()
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+        password: 'any_password'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(500)
+  })
+
+  test('should return 500 if AuthUseCase has no auth method', () => {
+    const sut = new LoginRouter({})
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+        password: 'any_password'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(500)
+  })
 })
