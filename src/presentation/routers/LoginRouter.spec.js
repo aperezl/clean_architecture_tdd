66ae8d1c6b8fc3b6903ef46a2d44f57f17a06ee8
Antonio Manuel Perez Lopez <aperezl@gmail.com>
2021-02-20 11:19:45 +0100

    ensure LoginRouter calls EmailValidator with correct email

--- src/presentation/routers/LoginRouter.spec.js
+++ src/presentation/routers/LoginRouter.spec.js
@@ -19,6 +19,7 @@ const makeSut = () => {
 const makeEmailValidator = () => {
   class EmailValidatorSpy {
     isValid (email) {
+      this.email = email
       return this.isEmailValid
     }
   }
@@ -231,5 +232,17 @@ describe('Login Router', () => {
     const httpResponse = await sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(500)
     expect(httpResponse.body).toEqual(new ServerError())
+  })
+
+  test('should call Email with correct email', async () => {
+    const { sut, emailValidatorSpy } = makeSut()
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+        password: 'any_password'
+      }
+    }
+    await sut.route(httpRequest)
+    expect(emailValidatorSpy.email).toBe(httpRequest.body.email)
   })
 })
