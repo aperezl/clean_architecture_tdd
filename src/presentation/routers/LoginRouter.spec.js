4c1add9259721ee83b83090c2b67cbf84f8db010
Antonio Manuel Perez Lopez <aperezl@gmail.com>
2021-02-20 11:16:11 +0100

    ensure LoginRouter return 500 if EmailValidator throws

--- src/presentation/routers/LoginRouter.spec.js
+++ src/presentation/routers/LoginRouter.spec.js
@@ -28,6 +28,15 @@ const makeEmailValidator = () => {
   return emailValidatorSpy
 }
 
+const makeEmailValidatorWithError = () => {
+  class EmailValidatorSpy {
+    isValid () {
+      throw new Error()
+    }
+  }
+  return new EmailValidatorSpy()
+}
+
 const makeAuthUseCase = () => {
   class AuthUseCaseSpy {
     async auth (email, password) {
@@ -198,6 +207,21 @@ describe('Login Router', () => {
   test('should return 500 if EmailValidator has not isValid', async () => {
     const authAuthUseCase = makeAuthUseCase()
     const sut = new LoginRouter(authAuthUseCase, {})
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+        password: 'any_password'
+      }
+    }
+    const httpResponse = await sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(500)
+    expect(httpResponse.body).toEqual(new ServerError())
+  })
+
+  test('should return 500 if EmailValidator throws', async () => {
+    const authUseCaseSpy = makeAuthUseCase()
+    const emailValidatorSpy = makeEmailValidatorWithError()
+    const sut = new LoginRouter(authUseCaseSpy, emailValidatorSpy)
     const httpRequest = {
       body: {
         email: 'any_email@mail.com',
