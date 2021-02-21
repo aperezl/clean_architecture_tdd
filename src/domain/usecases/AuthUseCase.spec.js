97235b2bc3b61e292e5a3d574e0992243182a6fb
Antonio Manuel Perez Lopez <aperezl@gmail.com>
2021-02-22 00:23:16 +0100

    ensure AuthUseCase throw if no email provided

---
+++ src/domain/usecases/AuthUseCase.spec.js
@@ -0,0 +1,15 @@
+class AuthUseCase {
+  async auth (email) {
+    if (!email) {
+      throw new Error()
+    }
+  }
+}
+
+describe('Auth UseCase', () => {
+  test('should throw if no email is provided', () => {
+    const sut = new AuthUseCase()
+    const promise = sut.auth()
+    expect(promise).rejects.toThrow()
+  })
+})
