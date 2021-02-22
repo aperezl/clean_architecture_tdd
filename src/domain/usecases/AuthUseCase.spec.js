cb67c1adf945c5a8b5a610ef516641acb795b850
Antonio Manuel Perez Lopez <aperezl@gmail.com>
2021-02-22 20:50:44 +0100

    ensure AuthUseCase calls LoadUserByRepository with correct email

--- src/domain/usecases/AuthUseCase.spec.js
+++ src/domain/usecases/AuthUseCase.spec.js
@@ -1,6 +1,10 @@
 import { MissingParamError } from '../../utils/errors'
 
 class AuthUseCase {
+  constructor (loadUserByEmailRepository) {
+    this.loadUserByEmailRepository = loadUserByEmailRepository
+  }
+
   async auth (email, password) {
     if (!email) {
       throw new MissingParamError('email')
@@ -8,6 +12,7 @@ class AuthUseCase {
     if (!password) {
       throw new MissingParamError('password')
     }
+    this.loadUserByEmailRepository.load(email)
   }
 }
 
@@ -22,5 +27,18 @@ describe('Auth UseCase', () => {
     const sut = new AuthUseCase()
     const promise = sut.auth('any_email@mail.com')
     expect(promise).rejects.toThrow(new MissingParamError('password'))
+  })
+
+  test('should call LoadUserByEmailRepository with correct email', async () => {
+    class LoadUserByEmailRepositorySpy {
+      async load (email) {
+        this.email = email
+      }
+    }
+    const loadUserByEmailRepository = new LoadUserByEmailRepositorySpy()
+
+    const sut = new AuthUseCase(loadUserByEmailRepository)
+    await sut.auth('any_email@mail.com', 'any_password')
+    expect(loadUserByEmailRepository.email).toBe('any_email@mail.com')
   })
 })
