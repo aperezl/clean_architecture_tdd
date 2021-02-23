6062e66abd8d2adbdaf1e6c71ffd43e1ecf009d8
Antonio Manuel Perez Lopez <aperezl@gmail.com>
2021-02-24 00:31:25 +0100

    ensure TokenGenerator throws if no id is provided

--- src/utils/helpers/TokenGenerator.spec.js
+++ src/utils/helpers/TokenGenerator.spec.js
@@ -11,6 +11,9 @@ class TokenGenerator {
     if (!this.secret) {
       throw new MissingParamError('secret')
     }
+    if (!id) {
+      throw new MissingParamError('id')
+    }
     return jwt.sign(id, this.secret)
   }
 }
@@ -44,5 +47,11 @@ describe('Token Generator', () => {
     const sut = new TokenGenerator()
     const promise = sut.generate('any_id')
     expect(promise).rejects.toThrow(new MissingParamError('secret'))
+  })
+
+  test('should throw if no id is provided', async () => {
+    const sut = makeSut()
+    const promise = sut.generate()
+    expect(promise).rejects.toThrow(new MissingParamError('id'))
   })
 })
