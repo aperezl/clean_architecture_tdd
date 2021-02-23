8433566c36aa6857f87c0b0cf781a0e678c2fbec
Antonio Manuel Perez Lopez <aperezl@gmail.com>
2021-02-24 00:33:57 +0100

    move TokenGenerator to its own file

---
+++ src/utils/helpers/TokenGenerator.js
@@ -0,0 +1,19 @@
+import jwt from 'jsonwebtoken'
+
+import { MissingParamError } from '../errors'
+
+export default class TokenGenrator {
+  constructor (secret) {
+    this.secret = secret
+  }
+
+  async generate (id) {
+    if (!this.secret) {
+      throw new MissingParamError('secret')
+    }
+    if (!id) {
+      throw new MissingParamError('id')
+    }
+    return jwt.sign(id, this.secret)
+  }
+}
--- src/utils/helpers/TokenGenerator.spec.js
+++ src/utils/helpers/TokenGenerator.spec.js
@@ -1,22 +1,7 @@
 import jwt from 'jsonwebtoken'
 
 import { MissingParamError } from '../errors'
-
-class TokenGenerator {
-  constructor (secret) {
-    this.secret = secret
-  }
-
-  async generate (id) {
-    if (!this.secret) {
-      throw new MissingParamError('secret')
-    }
-    if (!id) {
-      throw new MissingParamError('id')
-    }
-    return jwt.sign(id, this.secret)
-  }
-}
+import TokenGenerator from './TokenGenerator'
 
 const makeSut = () => {
   return new TokenGenerator('secret')
