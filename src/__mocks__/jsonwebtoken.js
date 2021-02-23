24cbf7dadc46437a66acfe712900061fb61ee5b6
Antonio Manuel Perez Lopez <aperezl@gmail.com>
2021-02-24 00:03:22 +0100

    ensure TokenGenerator returns a token if JWT returns token

---
+++ src/__mocks__/jsonwebtoken.js
@@ -0,0 +1,6 @@
+export default {
+  token: 'any_token',
+  sign (id, secret) {
+    return this.token
+  }
+}
--- src/utils/helpers/TokenGenerator.spec.js
+++ src/utils/helpers/TokenGenerator.spec.js
@@ -1,12 +1,21 @@
+import jwt from 'jsonwebtoken'
+
 class TokenGenerator {
   async generate (id) {
-    return null
+    return jwt.sign(id, 'secret')
   }
 }
 describe('Token Generator', () => {
   test('should return null if JWT returns null', async () => {
     const sut = new TokenGenerator()
+    jwt.token = null
     const token = await sut.generate('any_id')
     expect(token).toBeNull()
+  })
+
+  test('should return a token if JWT returns token', async () => {
+    const sut = new TokenGenerator()
+    const token = await sut.generate('any_id')
+    expect(token).toBe(jwt.token)
   })
 })
