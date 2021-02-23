45edd456e2d0df2f8af618a40e1d20b0d9890477
Antonio Manuel Perez Lopez <aperezl@gmail.com>
2021-02-23 23:37:33 +0100

    ensure TokenGenerator returns null if JWT returns null

---
+++ src/utils/helpers/TokenGenerator.spec.js
@@ -0,0 +1,12 @@
+class TokenGenerator {
+  async generate (id) {
+    return null
+  }
+}
+describe('Token Generator', () => {
+  test('should return null if JWT returns null', async () => {
+    const sut = new TokenGenerator()
+    const token = await sut.generate('any_id')
+    expect(token).toBeNull()
+  })
+})
