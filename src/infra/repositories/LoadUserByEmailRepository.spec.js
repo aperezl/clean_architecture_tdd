0a8cf8ebb8ddbce8b6afe88b495c427e33e83c0d
Antonio Manuel Perez Lopez <aperezl@gmail.com>
2021-02-26 23:05:40 +0100

    ensure LoadUserByEmailRepository throw if no userModel is provided

--- src/infra/repositories/LoadUserByEmailRepository.spec.js
+++ src/infra/repositories/LoadUserByEmailRepository.spec.js
@@ -49,4 +49,10 @@ describe('LoadUserByEmail Repository', () => {
 
     })
   })
+
+  test('should throw if no userModel is provided', async () => {
+    const sut = new LoadUserByEmailRepository()
+    const promise = sut.load('any_email@mail.com')
+    expect(promise).rejects.toThrow()
+  })
 })
