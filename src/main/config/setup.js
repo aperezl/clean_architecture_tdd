b29d2764d69a99c5dd2c7677f83563ffc8c51959
Antonio Manuel Perez Lopez <aperezl@gmail.com>
2021-02-27 12:49:38 +0100

    enable CORS

--- src/main/config/setup.js
+++ src/main/config/setup.js
@@ -1,3 +1,9 @@
 export default app => {
   app.disable('x-powered-by')
+  app.use((req, res, next) => {
+    res.set('access-control-allow-origin', '*')
+    res.set('access-control-allow-methods', '*')
+    res.set('access-control-allow-headers', '*')
+    next()
+  })
 }
