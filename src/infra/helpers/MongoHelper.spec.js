c9013a0ac53cca85850bd7d70ec604ecc48ffffd
Antonio Manuel Perez Lopez <aperezl@gmail.com>
2021-02-28 17:55:41 +0100

    disconnect mongodb after MongoHelper test

--- src/infra/helpers/MongoHelper.spec.js
+++ src/infra/helpers/MongoHelper.spec.js
@@ -1,13 +1,14 @@
-import MongoHelper from './MongoHelper'
+import sut from './MongoHelper'
 
 describe('Mongo Helper', () => {
+  beforeAll(async () => await sut.connect(process.env.MONGO_URL))
+  afterAll(async () => await sut.disconnect())
+
   test('should reconnect when getDb() is invoked and client is disconnected', async () => {
-    const sut = MongoHelper
-    await sut.connect(process.env.MONGO_URL)
     expect(sut.db).toBeTruthy()
     await sut.disconnect()
     expect(sut.db).toBeFalsy()
-    expect(await sut.getDb()).toBeTruthy()
-    await sut.disconnect()
+    await sut.getDb()
+    expect(sut.db).toBeTruthy()
   })
 })
