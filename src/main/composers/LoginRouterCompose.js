3e8dec5da5dbdf78ecd4a14e525f66430afb9e6d
Antonio Manuel Perez Lopez <aperezl@gmail.com>
2021-03-01 18:47:15 +0100

    move LoginRouter composition to its own file

---
+++ src/main/composers/LoginRouterCompose.js
@@ -0,0 +1,23 @@
+import AuthUseCase from '../../domain/usecases/AuthUseCase'
+import LoadUserByEmailRepository from '../../infra/repositories/LoadUserByEmailRepository'
+import UpdateAccessTokenRepository from '../../infra/repositories/UpdateAccessTokenRepository'
+import env from '../../main/config/env'
+import LoginRouter from '../../presentation/routers/LoginRouter'
+import EmailValidator from '../../utils/helpers/EmailValidator'
+import Encrypter from '../../utils/helpers/Encrypter'
+import TokenGenerator from '../../utils/helpers/TokenGenerator'
+
+const tokenGenerator = new TokenGenerator(env.tokekSecret)
+const encrypter = new Encrypter()
+const loadUserByEmailRepository = new LoadUserByEmailRepository()
+const updateAccessTokenRepository = new UpdateAccessTokenRepository()
+const emailValidator = new EmailValidator()
+const authUseCase = new AuthUseCase({
+  loadUserByEmailRepository,
+  updateAccessTokenRepository,
+  encrypter,
+  tokenGenerator
+})
+const loginRouter = new LoginRouter({ authUseCase, emailValidator })
+
+export default loginRouter
--- src/main/routes/LoginRoutes.js
+++ src/main/routes/LoginRoutes.js
@@ -1,25 +1,5 @@
-import AuthUseCase from '../../domain/usecases/AuthUseCase'
-import LoadUserByEmailRepository from '../../infra/repositories/LoadUserByEmailRepository'
-import UpdateAccessTokenRepository from '../../infra/repositories/UpdateAccessTokenRepository'
-import env from '../../main/config/env'
-import LoginRouter from '../../presentation/routers/LoginRouter'
-import EmailValidator from '../../utils/helpers/EmailValidator'
-import Encrypter from '../../utils/helpers/Encrypter'
-import TokenGenerator from '../../utils/helpers/TokenGenerator'
+import loginRouter from '../composers/LoginRouterCompose'
 
 export default async router => {
-  const tokenGenerator = new TokenGenerator(env.tokekSecret)
-  const encrypter = new Encrypter()
-  const loadUserByEmailRepository = new LoadUserByEmailRepository()
-  const updateAccessTokenRepository = new UpdateAccessTokenRepository()
-  const emailValidator = new EmailValidator()
-  const authUseCase = new AuthUseCase({
-    loadUserByEmailRepository,
-    updateAccessTokenRepository,
-    encrypter,
-    tokenGenerator
-  })
-  const loginRouter = new LoginRouter({ authUseCase, emailValidator })
-  console.log(loginRouter)
   router.post('/login', loginRouter)
 }
